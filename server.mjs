657b446e6935ee60675e1c9c6609b28865777afd
aqibrind <92119956+aqibrind@users.noreply.github.com>
2021-11-01 23:00:38 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,25 +1,88 @@
 import express from 'express';
-const app = express()
+import morgan from 'morgan';
+
 const port = process.env.PORT || 3000
 
-app.use((req,res,next)=>{
-    console.log("a request came", Date.now());
+let users= [];
+app.use (express.json())
+app.use(morgan('short'))
+
+app.use((req, res, next) =>{
+    console.log("a request came", req.body);
     next()
 })
 
-app.get('/profile', (req, res) => {
-  res.send('here is your profile')
+ap.use((req, res,next) => {
+    console.log("a request",Data.now())
 })
-app.get('/home', (req, res) => {
+
+app.get('/users' ,(req,res)=>{
+    res.send(users)
+})
+
+app.get('/users/:id' ,(req,res)=>{
+    if(user[req.params.id]){
+        res.send(user[req.params.id])
+    }else{
+        req.send("user not found");
+    }
+})
+app.post('/user', (req, res) => {
+
+    if (!req.body.name || !req.body.email || !req.body.address) {
+      res.status(400).send("invalid data");
+    } else {
+      users.push({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address
+      })
+  
+      res.send("users created");
+    }
+  })
+  app.put('/user/:id', (req, res) => {
+  
+    if (users[req.params.id]) {
+  
+      if (req.body.name) {
+        users[req.params.id].name = req.body.name
+      }
+      if (req.body.email) {
+        users[req.params.id].email = req.body.email
+      }
+      if (req.body.address) {
+        users[req.params.id].address = req.body.address
+      }
+  
+      res.send(users[req.params.id])
+  
+    } else {
+      res.send("user not found");
+    }
+  })
+  
+  app.delete('/user/:id', (req, res) => {
+  
+    if (users[req.params.id]) {
+  
+      users[req.params.id] = {};
+      res.send("user deleted");
+  
+    } else {
+      res.send("user not found");
+    }
+  })
+  
+  app.get('/home', (req, res) => {
     res.send('here is your home')
-})
-app.get('/', (req, res) => {
+  })
+  app.get('/', (req, res) => {
     res.send('Hi I am a hello world Server program')
-})
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-// deploy this server to heroku cloud
-// read: https://devcenter.heroku.com/articles/getting-started-with-nodejs
